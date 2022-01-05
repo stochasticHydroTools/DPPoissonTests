8122ecb26da7f0bd0e3bf1056d7690834e344f62
RaulPPealez <raul.perez@uam.es>
2022-01-05 12:00:01 -0500

    Fix non-compiland ** symbol (^ must be used) in awk script

--- charged_wall/test.bash
+++ charged_wall/test.bash
@@ -16,5 +16,5 @@ bash tools/computeDensityProfile.sh results/pos.dat $K > results/density.dat
 bash tools/generateTheoreticalDensityProfile.sh $K > results/density.theo
 
 H=$(grep -Eo "^H[[:space:]].*" data.main | awk '{print $2}')
-meanError=$(paste results/den* | awk '$4&&$1<('$H'*0.5-4.0){m+=sqrt(($2-$4)**2)/$4;c++}END{print m/c}')
+meanError=$(paste results/den* | awk '$4&&$1<('$H'*0.5-4.0){m+=sqrt(($2-$4)^2)/$4;c++}END{print m/c}')
 echo "Mean relative error (deviation from PNP theory far from wall): " $meanError
